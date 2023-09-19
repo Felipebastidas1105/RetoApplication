9a8f328cf5588b56e47ff47e2a974697b2c36ed5
Juan Felipe Bastidas Bedoya <juanfelipebastidas1105@gmail.com>
2023-09-19 13:51:17 -0500

    se agregaron cambios para poder regenerar el codigo y se agrego la pantalla de registro pero esta a falta de detalles

--- app/src/main/java/com/example/retoapplication/GenerateCodeActivity.kt
+++ app/src/main/java/com/example/retoapplication/GenerateCodeActivity.kt
@@ -1,40 +1,53 @@
 package com.example.retoapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.widget.Button
 import android.widget.TextView
 import kotlin.random.Random
 
 class GenerateCodeActivity : AppCompatActivity() {
     private lateinit var tvCode:TextView
     private lateinit var tvCont:TextView
-    var second:Long = 5 * 1000
+    private lateinit var btnNext:Button
+    var second:Long = 10 * 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generate_code)
         tvCode = findViewById(R.id.tvText2)
         tvCont = findViewById(R.id.tvText4)
-        tvCode.text =   generarNumeroAleatorio()
-        cuentaRegresiva()
-
+        btnNext = findViewById(R.id.button)
+        tvCode.text = generateRandomNumber()
+        regressiveCount()
+        btnNext.setOnClickListener {changeView()}
     }
 
-    fun generarNumeroAleatorio(): String {
-        val numeroAleatorio = Random.nextInt(10000, 100000)
-        return String.format("%05d", numeroAleatorio)
+    fun generateRandomNumber(): String {
+        val randomNumber = Random.nextInt(10000, 100000)
+        return String.format("%05d", randomNumber)
     }
-    private fun cuentaRegresiva() {
+    private fun regressiveCount() {
         tvCont.text = (second /1000).toString()
         var countDownTimer = object : CountDownTimer(second, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                tvCont.text = (millisUntilFinished /1000).toString()
+                tvCont.text = (millisUntilFinished /1000).toString() + "s"
             }
 
             override fun onFinish() {
-                generarNumeroAleatorio()
+                tvCode.text = generateRandomNumber()
+                regressiveCount()
             }
         }
         countDownTimer.start()
     }
+    private fun changeView(){
+
+            val intent = Intent(this,RegisterActivity::class.java)
+            intent.putExtra("EXTRA_CODE", tvCode.toString())
+            startActivity(intent)
+
+    }
+
 }
